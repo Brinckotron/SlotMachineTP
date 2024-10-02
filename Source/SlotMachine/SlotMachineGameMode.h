4cd9c545675be7923447678a70a84dcadc14f5f4
François Brinck-Boucher <146009172+Brinckotron@users.noreply.github.com>
2024-10-02 19:41:08 -0400

    Added Unreal Project

---
+++ Source/SlotMachine/SlotMachineGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SlotMachineGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASlotMachineGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASlotMachineGameMode();
+};
+
+
+
